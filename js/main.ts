ccb5904e2ec839d11e7b531a2da1d087d12f552c
Kay280 <kche584@aucklanduni.ac.nz>
2016-09-24 20:12:53 +1200

    Update main.ts

--- js/main.ts
+++ js/main.ts
@@ -6,12 +6,17 @@ $(document).ready(function() {
     $('#plugin2').hover(function() {
         $(this).fadeOut('fast');
     });
+
+    $('.pull-me').click(function () {
+        
+       $('.panel').slideToggle('slow'); 
+        
+        
+    });
     
-});
+});   
 
-   
-
-$("button").click(function(){
+/*$("button").click(function(){
     $.getJSON("http://api.fixer.io/latest", function(result){
         console.log(result);
         $.each(result.rates, function(i, field){
@@ -19,5 +24,85 @@ $("button").click(function(){
             $("div#output").append(i + " " + field + " ");
         });
     });
+});*/
+var buttonOutputArea = $("div#output");
+var allRates = $('#all');
+
+$('#b1').click(function() : void{
+
+    $.ajax({
+        type: "GET",
+        url: "http://api.fixer.io/latest",
+
+         success: function (data) {
+            //console.log("success", data);
+            $.each(data.rates, function (i, item) {
+
+                allRates.append(i + " " + item + " ");
+                
+                if(i === "NZD"){
+                    buttonOutputArea.append(i + " " + item);
+                }
+            });
+        }
+
+    });
+
+$("#search").on("click", function() : void{
+  var searchTerm = $("#searchTerm").val();
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+  $.ajax({
+    url: url,
+    type: "GET",
+    async: false,
+    dataType: "json",
+    success: function(data, status, jqXHR){
+      // console.log(data);
+      for(var i = 0; i < data[1].length; i++) {
+        $("#wikiOutput").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+        
+      }
+    }
+    
+  });
+  
+});    
+
+
+
 });
 
+
+
+
+
+
+
+/*class website {
+
+    url:string;
+
+    facebookLikes:number;
+
+    constructor(url:string, facebookLikes:number){
+
+        this.url = url;
+        this.facebookLikes = facebookLikes;
+    }
+}
+
+var google = new website("google.com", 9);*/
+
+
+
+/*class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+var greeter = new Greeter("world");*/
